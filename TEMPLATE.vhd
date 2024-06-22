c3e70f52a28d44d59635f637768057027cdffd00
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-06-21 22:16:38 -0300

    Plantilla
    Plantilla para evitar reescribir lo de siempre, extraído de la Teoría Diapositiva 76 Guia 3 Algebra de Boole.

---
+++ TEMPLATE.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use	IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity TEMPLATE is
+	port (B : in STD_LOGIC;
+		  A	: in STD_LOGIC;
+		  Z : out STD_LOGIG);
+end TEMPLATE;
+
+architecture RTL of TEMPLATE is
+
+begin
+
+end RTL;
