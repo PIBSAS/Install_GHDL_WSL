bc9c08141c7139e8885737bf9ca9ead9401b36ae
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-06-24 22:02:35 -0300

    Update raspberry.sh

--- raspberry.sh
+++ raspberry.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 sudo apt update
 git clone https://github.com/ghdl/ghdl.git
-sudo apt install -y zlib1g-dev gcc make clang llvm gnat
+sudo apt install -y zlib1g-dev gcc make clang llvm gnat wget unzip openjdk-17-jdk gtkwave
 cd ghdl
 ./configure --with-llvm-config
 make
 sudo make install
 ghdl --version
+cd
 echo "Obtenemos Digital"
 cd
 wget -c https://github.com/hneemann/Digital/releases/latest/download/Digital.zip
