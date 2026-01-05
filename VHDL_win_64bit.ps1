f9b9634c5af8e521f983476ae886ce436a4a782f
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2026-01-05 15:35:52 -0300

    Update 2026 para todas las plataformas

--- VHDL_win_64bit.ps1
+++ VHDL_win_64bit.ps1
@@ -5,7 +5,7 @@ cd $HOME\Downloads\
 
 Write-Host "Descargar y extraer GHDL"
 wget "https://github.com/ghdl/ghdl/releases/download/v5.1.1/ghdl-mcode-5.1.1-ucrt64.zip" -OutFile ghdl-UCRT64.zip
-Expand-Archive ghdl*.zip -DestinationPath C:\
+Expand-Archive ghdl*.zip -DestinationPath C:\ghdl
 Remove-Item ghdl-UCRT64.zip
 cd ..
 
@@ -27,4 +27,5 @@ cd ..
 Write-Host "Instalar Java 8 y Notepad++ usando winget"
 winget install "Java 8" --accept-package-agreements --silent
 winget install "notepad++" --accept-package-agreements --silent
+
 
